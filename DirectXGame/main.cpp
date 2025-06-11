1820d0a75385437fa01815fbc71e8abe9651b386
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2025-06-11 11:44:00 +0900

    Created IndexBuffer Class

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -4,6 +4,7 @@
 #include "RootSignature.h"
 #include "Shader.h"
 #include "VertexBuffer.h"
+#include "IndexBuffer.h"
 #include <Windows.h>
 
 using namespace KamataEngine;
@@ -92,16 +93,42 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	PipelineState pipelineState;
 	SetupPipelineState(pipelineState, rs, vs, ps);
 
+	 //リソースの確保含め、頂点情報を柔軟に対応できるようにVertexData構造体を新たに作成する
+	 //Vector4 → VertexDataに変更して利用する
+	struct VertexData {
+		Vector4 position;
+	};
+
+	// 頂点データの準備　*00_07 追加
+	VertexData vertices[] = {
+		{0.0f,0.5f,0.0f,1.0f},
+		{0.5f,-0.5f,0.0f,1.0f},
+		{-0.5f,-0.5f,0.0f,1.0f},
+	};
+
 	// VertexBufferの生成
 	VertexBuffer vb;
-	vb.Create(sizeof(Vector4) * 3, sizeof(Vector4));
+	vb.Create(sizeof(vertices), sizeof(vertices[0]));
 
 	// 頂点リソースにデータを書き込む
-	Vector4* vertexData = nullptr;
-	vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-	vertexData[0] = Vector4(-0.5f, -0.5f, 0.0f, 1.0f);
-	vertexData[1] = Vector4(0.0f, 0.5f, 0.0f, 1.0f);
-	vertexData[2] = Vector4(0.5f, -0.5f, 0.0f, 1.0f);
+	VertexData* pGpuVertices = nullptr;
+	vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&pGpuVertices));
+	for (int i = 0; i < _countof(vertices); ++i) {
+		pGpuVertices[i] = vertices[i];
+	}
+
+	uint16_t indices[] = {0, 2, 1};
+
+	// IndexBufferの生成
+	IndexBuffer ib;
+	ib.Create(sizeof(indices), sizeof(indices[0]));
+
+	// 頂点インデックスリソースにデータを書き込む
+	uint16_t* pGpuIndices = nullptr;
+	ib.Get()->Map(0, nullptr, reinterpret_cast<void**>(&pGpuIndices));
+	for (int i = 0; i < _countof(indices); ++i) {
+		pGpuIndices[i] = indices[i];
+	}
 
 	// ゲームシーンのインスタンスの生成
 	GameScene* gameScene = new GameScene();
@@ -126,10 +153,12 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		commandList->SetGraphicsRootSignature(rs.Get());
 		commandList->SetPipelineState(pipelineState.Get());
 		commandList->IASetVertexBuffers(0, 1, vb.GetView());
+		commandList->IASetIndexBuffer(ib.GetView());
 		// トポロジの設定
 		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 		// 頂点数、インデックス数、インデックスの開始位置、インデックスのオフセット
-		commandList->DrawInstanced(3, 1, 0, 0);
+		//commandList->DrawInstanced(3, 1, 0, 0);
+		commandList->DrawIndexedInstanced(_countof(indices), 1, 0, 0, 0);
 
 		// ゲームシーンの描画
 		gameScene->Draw();
