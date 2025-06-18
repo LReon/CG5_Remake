e6298d2224985e950d403a98d4d752b1c1e0edd9
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2025-06-18 12:05:32 +0900

    Completed

--- DirectXGame/Resources/shaders/TestVS.hlsl
+++ DirectXGame/Resources/shaders/TestVS.hlsl
@@ -1,16 +1,16 @@
-struct VertexShaderOutput
-{
-    float4 position : SV_POSITION;
-};
+#include "Test.hlsli"
+
 
 struct VertexShaderInput
 {
     float32_t4 position : POSITION0;
+    float32_t2 texCoord : TEXCOORD0;
 };
 
 VertexShaderOutput main(VertexShaderInput input)
 {
     VertexShaderOutput output;
     output.position = input.position;
+    output.texCoord = input.texCoord;
     return output;
 }
